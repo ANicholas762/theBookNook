366c61e9b4a2dcc32961f61015954ebfca3ff34b
Denisse03 <99741771+Denisse03@users.noreply.github.com>
2022-10-12 16:10:10 -0400

    changes

---
+++ app/model.js
@@ -0,0 +1,463 @@
+var homeContent = ``;
+var aboutContent = ``;
+var booksContent = ` <div class="novels">
+<h3>BOOKSETS</h3>
+</div>
+
+<div class="allnovel">
+<div class="novelinfo">
+  <div class="novel-info">
+    <div class="novel-image"></div>
+    <div class="text-novel">
+      <div class="novel-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Netus et malesuada fames ac.
+      </div>
+      <div class="price">
+        <h6>$99.99</h6>
+      </div>
+      <button type="button">ADD TO CART</button>
+    </div>
+  </div>
+</div>
+
+<div class="novelinfo">
+  <div class="novel-info">
+    <div class="novel-imaged"></div>
+    <div class="text-novel">
+      <div class="novel-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Netus et malesuada fames ac.
+      </div>
+      <div class="price">
+        <h6>$100</h6>
+      </div>
+      <button type="button">ADD TO CART</button>
+    </div>
+  </div>
+</div>
+
+<div class="novelinfo">
+  <div class="novel-info">
+    <div class="novel-images"></div>
+    <div class="text-novel">
+      <div class="novel-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Netus et malesuada fames ac.
+      </div>
+      <div class="price">
+        <h6>$100</h6>
+      </div>
+      <button type="button">ADD TO CART</button>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="novels">
+<h3>BLACK HISTORY BOOKS</h3>
+</div>
+
+<div class="allnovel">
+<div class="novelinfo">
+  <div class="novel-info">
+    <div class="novel-imag"></div>
+    <div class="text-novel">
+      <div class="novel-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Netus et malesuada fames ac.
+      </div>
+      <div class="price">
+        <h6>$27.99</h6>
+      </div>
+      <button type="button">ADD TO CART</button>
+    </div>
+  </div>
+</div>
+
+<div class="novelinfo">
+  <div class="novel-info">
+    <div class="novel-imags"></div>
+    <div class="text-novel">
+      <div class="novel-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Netus et malesuada fames ac.
+      </div>
+      <div class="price">
+        <h6>$19.99</h6>
+      </div>
+      <button type="button">ADD TO CART</button>
+    </div>
+  </div>
+</div>
+
+<div class="novelinfo">
+  <div class="novel-info">
+    <div class="novel-imagsd"></div>
+    <div class="text-novel">
+      <div class="novel-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Netus et malesuada fames ac.
+      </div>
+      <div class="price">
+        <h6>$17.99</h6>
+      </div>
+      <button type="button">ADD TO CART</button>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="novels">
+<h3>HORROR BOOKS</h3>
+</div>
+<div class="allnovel">
+<div class="novelinfo">
+  <div class="novel-info">
+    <div class="novel-pic"></div>
+    <div class="text-novel">
+      <div class="novel-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Netus et malesuada fames ac.
+      </div>
+      <div class="price">
+        <h6>$19.99</h6>
+      </div>
+      <button type="button">ADD TO CART</button>
+    </div>
+  </div>
+</div>
+
+<div class="novelinfo">
+  <div class="novel-info">
+    <div class="novel-pics"></div>
+    <div class="text-novel">
+      <div class="novel-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Netus et malesuada fames ac.
+      </div>
+      <div class="price">
+        <h6>$15.99</h6>
+      </div>
+      <button type="button">ADD TO CART</button>
+    </div>
+  </div>
+</div>
+
+<div class="novelinfo">
+  <div class="novel-info">
+    <div class="novel-photo"></div>
+    <div class="text-novel">
+      <div class="novel-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Netus et malesuada fames ac.
+      </div>
+      <div class="price">
+        <h6>$19.99</h6>
+      </div>
+      <button type="button">ADD TO CART</button>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="novels">
+<h3>CHILDRENS BOOKS</h3>
+</div>
+
+<div class="allnovel">
+<div class="novelinfo">
+  <div class="novel-info">
+    <div class="novel-photo2"></div>
+    <div class="text-novel">
+      <div class="novel-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Netus et malesuada fames ac.
+      </div>
+      <div class="price">
+        <h6>$19.99</h6>
+      </div>
+      <button type="button">ADD TO CART</button>
+    </div>
+  </div>
+</div>
+
+<div class="novelinfo">
+  <div class="novel-info">
+    <div class="novel-photo3"></div>
+    <div class="text-novel">
+      <div class="novel-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Netus et malesuada fames ac.
+      </div>
+      <div class="price">
+        <h6>$15.99</h6>
+      </div>
+      <button type="button">ADD TO CART</button>
+    </div>
+  </div>
+</div>
+
+<div class="novelinfo">
+  <div class="novel-info">
+    <div class="novel-photo4"></div>
+    <div class="text-novel">
+      <div class="novel-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Netus et malesuada fames ac.
+      </div>
+      <div class="price">
+        <h6>$7.99</h6>
+      </div>
+      <button type="button">ADD TO CART</button>
+    </div>
+  </div>
+</div>
+</div>`;
+var blogContent = ``;
+var accountContent = ``;
+var cartContent = ``;
+var febContent = `<div class="feb">
+<div class="banner">
+  <div class="sign">
+    <p>FEBRUARY FAVORITES</p>
+    <div class="text">In Honor of Black History Month</div>
+
+    <div class="quote">
+      "Success is only meaningful and enjoyable if it feels like your own"
+      - Michelle Obama
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="container">
+<div class="image">
+  <img src="images/becoming.jpg" alt="" />
+</div>
+<div class="textthree">
+  <h1>Becoming By Michelle Obama</h1>
+
+  <div class="textfour">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Non arcu
+    risus quis varius. Non consectetur a erat nam at lectus urna duis.
+    Gravida arcu ac tortor dignissim. Diam phasellus vestibulum lorem sed
+    risus ultricies. Tortor condimentum lacinia quis vel eros donec ac
+    odio tempor. Lorem ipsum dolor sit amet consectetur adipiscing elit
+    duis. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu
+    vitae. Arcu risus quis varius quam quisque id diam vel quam. Amet
+    cursus sit amet dictum sit.
+  </div>
+
+  <div class="textfour">
+    Ultricies integer quis auctor elit sed vulputate mi sit amet.
+    Elementum integer enim neque volutpat ac. Id neque aliquam vestibulum
+    morbi blandit cursus risus. Diam phasellus vestibulum lorem sed risus
+    ultricies tristique nulla aliquet. Elementum nibh tellus molestie nunc
+    non. Eleifend mi in nulla posuere sollicitudin aliquam. Euismod
+    lacinia at quis risus sed vulputate. Tellus molestie nunc non blandit
+    massa enim nec dui nunc. Vulputate eu scelerisque felis imperdiet
+    proin fermentum leo. Tellus mauris a diam maecenas sed. Aenean et
+    tortor at risus viverra adipiscing at in. Ipsum suspendisse ultrices
+    gravida dictum fusce. Arcu odio ut sem nulla..
+  </div>
+</div>
+</div>
+
+<div class="container">
+<div class="image">
+  <img src="images/finding-me.jpg" alt="" />
+</div>
+<div class="textthree">
+  <h1>Finding Me By Viola Davis</h1>
+
+  <div class="textfour">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Non arcu
+    risus quis varius. Non consectetur a erat nam at lectus urna duis.
+    Gravida arcu ac tortor dignissim. Diam phasellus vestibulum lorem sed
+    risus ultricies. Tortor condimentum lacinia quis vel eros donec ac
+    odio tempor. Lorem ipsum dolor sit amet consectetur adipiscing elit
+    duis. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu
+    vitae. Arcu risus quis varius quam quisque id diam vel quam. Amet
+    cursus sit amet dictum sit.
+  </div>
+
+  <div class="textfour">
+    Ultricies integer quis auctor elit sed vulputate mi sit amet.
+    Elementum integer enim neque volutpat ac. Id neque aliquam vestibulum
+    morbi blandit cursus risus. Diam phasellus vestibulum lorem sed risus
+    ultricies tristique nulla aliquet. Elementum nibh tellus molestie nunc
+    non. Eleifend mi in nulla posuere sollicitudin aliquam. Euismod
+    lacinia at quis risus sed vulputate. Tellus molestie nunc non blandit
+    massa enim nec dui nunc. Vulputate eu scelerisque felis imperdiet
+    proin fermentum leo. Tellus mauris a diam maecenas sed. Aenean et
+    tortor at risus viverra adipiscing at in. Ipsum suspendisse ultrices
+    gravida dictum fusce. Arcu odio ut sem nulla..
+  </div>
+</div>
+</div>`;
+var bookclubContent = ` <div class="book">
+<div class="banners">
+  <div class="about">
+    <p>ALL ABOUT BOOKCLUBS</p>
+    <div class="books">
+      What books are best for book club? Everything you need to know
+      about hosting your own!
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="bookimage">
+<div class="glasses">
+  <div class="pic"></div>
+
+  <div class="sent">
+    <div class="sen">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis
+      in eu mi bibendum neque egestas. Nunc lobortis mattis aliquam
+      faucibus purus in massa tempor. Diam in arcu cursus euismod quis
+      viverra nibh. Massa sed elementum tempus egestas sed sed risus
+      pretium. Aliquet eget sit amet tellus cras adipiscing enim eu.
+      Viverra nibh cras pulvinar mattis nunc sed blandit libero. Auctor
+      urna nunc id cursus metus. Mauris a diam maecenas sed enim ut.
+      Venenatis lectus magna fringilla urna porttitor rhoncus dolor
+      purus. Etiam non quam lacus suspendisse. Pulvinar elementum
+      integer enim neque volutpat ac tincidunt. Dolor morbi non arcu
+      risus. Maecenas volutpat blandit aliquam etiam erat velit
+      scelerisque. Ipsum nunc aliquet bibendum enim facilisis gravida
+      neque convallis a. Scelerisque purus semper eget duis at tellus
+      at.
+    </div>
+
+    <div class="sentwo">
+      Ornare arcu dui vivamus arcu felis. Risus at ultrices mi tempus
+      imperdiet. In cursus turpis massa tincidunt dui. Vitae suscipit
+      tellus mauris a diam maecenas. Dictumst quisque sagittis purus sit
+      amet volutpat. Varius morbi enim nunc faucibus a pellentesque.
+      Volutpat sed cras ornare arcu. Dictum varius duis at consectetur
+      lorem donec. In nibh mauris cursus mattis molestie a iaculis at.
+      Sit amet commodo nulla facilisi. Quam adipiscing vitae proin
+      sagittis. Egestas sed tempus urna et pharetra. Auctor elit sed
+      vulputate mi sit.
+    </div>
+
+    <div class="senthree">
+      Elit scelerisque mauris pellentesque pulvinar pellentesque. Vel
+      orci porta non pulvinar neque laoreet suspendisse interdum.
+      Accumsan in nisl nisi scelerisque eu ultrices. Ligula ullamcorper
+      malesuada proin libero nunc consequat interdum varius. At ultrices
+      mi tempus imperdiet nulla malesuada pellentesque. Enim ut sem
+      viverra aliquet. Feugiat vivamus at augue eget arcu dictum. Leo
+      vel orci porta non pulvinar. Enim blandit volutpat maecenas
+      volutpat. Egestas maecenas pharetra convallis posuere morbi leo.
+    </div>
+
+    <div class="senfour">
+      Sed tempus urna et pharetra pharetra massa massa ultricies. In
+      nibh mauris cursus mattis molestie a iaculis. Eu non diam
+      phasellus vestibulum lorem. Quis lectus nulla at volutpat diam ut.
+      Mattis rhoncus urna neque viverra justo. Morbi tincidunt augue
+      interdum velit euismod in. Habitasse platea dictumst quisque
+      sagittis. Malesuada fames ac turpis egestas maecenas pharetra
+      convallis posuere morbi. Placerat in egestas erat imperdiet sed. A
+      condimentum vitae sapien pellentesque. Pretium viverra suspendisse
+      potenti nullam ac tortor vitae. Ut diam quam nulla porttitor massa
+      id neque. Sodales ut etiam sit amet nisl purus in. Nulla facilisi
+      morbi tempus iaculis urna id volutpat lacus laoreet.
+    </div>
+  </div>
+</div>
+</div>`;
+var ereadingContent = `  <div class="reading">
+<div class="poster">
+  <div class="postertext">
+    <div class="postertexts">SWITCHING TO E-READING?</div>
+
+    <div class="reader">
+      What e-readers are best for you? everything you need to know about
+      electronic reading!
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="tabletimage">
+<div class="mug">
+  <div class="picture"></div>
+
+  <div class="send">
+    <div class="sendone">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis in
+      eu mi bibendum neque egestas. Nunc lobortis mattis aliquam faucibus
+      purus in massa tempor. Diam in arcu cursus euismod quis viverra nibh.
+      Massa sed elementum tempus egestas sed sed risus pretium. Aliquet eget
+      sit amet tellus cras adipiscing enim eu. Viverra nibh cras pulvinar
+      mattis nunc sed blandit libero. Auctor urna nunc id cursus metus.
+      Mauris a diam maecenas sed enim ut. Venenatis lectus magna fringilla
+      urna porttitor rhoncus dolor purus. Etiam non quam lacus suspendisse.
+      Pulvinar elementum integer enim neque volutpat ac tincidunt. Dolor
+      morbi non arcu risus. Maecenas volutpat blandit aliquam etiam erat
+      velit scelerisque. Ipsum nunc aliquet bibendum enim facilisis gravida
+      neque convallis a. Scelerisque purus semper eget duis at tellus at.
+    </div>
+
+    <div class="sendone">
+      Ornare arcu dui vivamus arcu felis. Risus at ultrices mi tempus
+      imperdiet. In cursus turpis massa tincidunt dui. Vitae suscipit tellus
+      mauris a diam maecenas. Dictumst quisque sagittis purus sit amet
+      volutpat. Varius morbi enim nunc faucibus a pellentesque. Volutpat sed
+      cras ornare arcu. Dictum varius duis at consectetur lorem donec. In
+      nibh mauris cursus mattis molestie a iaculis at. Sit amet commodo
+      nulla facilisi. Quam adipiscing vitae proin sagittis. Egestas sed
+      tempus urna et pharetra. Auctor elit sed vulputate mi sit.
+    </div>
+
+    <div class="sendone">
+      Elit scelerisque mauris pellentesque pulvinar pellentesque. Vel orci
+      porta non pulvinar neque laoreet suspendisse interdum. Accumsan in
+      nisl nisi scelerisque eu ultrices. Ligula ullamcorper malesuada proin
+      libero nunc consequat interdum varius. At ultrices mi tempus imperdiet
+      nulla malesuada pellentesque. Enim ut sem viverra aliquet. Feugiat
+      vivamus at augue eget arcu dictum. Leo vel orci porta non pulvinar.
+      Enim blandit volutpat maecenas volutpat. Egestas maecenas pharetra
+      convallis posuere morbi leo.
+    </div>
+
+    <div class="sendone">
+      Sed tempus urna et pharetra pharetra massa massa ultricies. In nibh
+      mauris cursus mattis molestie a iaculis. Eu non diam phasellus
+      vestibulum lorem. Quis lectus nulla at volutpat diam ut. Mattis
+      rhoncus urna neque viverra justo. Morbi tincidunt augue interdum velit
+      euismod in. Habitasse platea dictumst quisque sagittis. Malesuada
+      fames ac turpis egestas maecenas pharetra convallis posuere morbi.
+      Placerat in egestas erat imperdiet sed. A condimentum vitae sapien
+      pellentesque. Pretium viverra suspendisse potenti nullam ac tortor
+      vitae. Ut diam quam nulla porttitor massa id neque. Sodales ut etiam
+      sit amet nisl purus in. Nulla facilisi morbi tempus iaculis urna id
+      volutpat lacus laoreet.
+    </div>
+  </div>
+</div>
+</div>`;
+
+export function changePage(pageId) {
+  let page = pageId + "Content";
+  $("#app").html(eval(page));
+}
