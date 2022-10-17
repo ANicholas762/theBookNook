8665306f1ad335b27c3f645c6bccf9eed55d7904
ANicholas762 <84145964+ANicholas762@users.noreply.github.com>
2022-10-16 21:48:55 -0400

    app.js

--- app/app.js
+++ app/app.js
@@ -31,7 +31,63 @@ function initApp() {
   route();
 }
 
+function init() {
+  $("#login").on("click", function(e) {
+      console.log("on");
+
+      e.preventDefault();
+
+      // Login information
+      let em_one = $("#lemail").val();
+      let pw_one = $("#lpword").val();
+
+
+      if(em_one == "") {
+          alert("Please enter your email")
+          console.log("email_one");
+      }else if (pw_one == "") {
+          alert("Please enter your password")
+          console.log("password_one");
+      }else{
+          alert("You have successfully logged in")
+          console.log("success");
+      } 
+  });
+}
+
+function initTwo() {
+  $("#signup").on("click", function(e) {
+      console.log("on");
+
+      e.preventDefault();
+
+      // sign up information
+      let fn = $("#fn").val();
+      let ln = $("#ln").val();
+      let ea = $("#ea").val();
+      let pw_two = $("#pw").val();
+
+
+      if (fn == "") {
+          alert("Please enter your first name")
+      }else if (ln == "") {
+          alert("Please enter your last name")
+      }else if (ea == "") {
+          alert("Please enter your email address")
+      }else if (pw_two == "") {
+          alert("Please enter your password")
+      }else{
+          alert("You have successfully created an account" )
+
+            console.log("success");
+      }
+      
+  });
+}
+
 $(document).ready(function () {
   initApp();
   initListeners();
+  init();
+  initTwo();
 });
